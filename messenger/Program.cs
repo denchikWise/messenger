336bde6e0fe767a013c0ca585a64472576899d3e
denchikWise <wassly.time@mail.ru>
2024-02-15 17:01:58 +0500

    Добавьте файлы проекта.

---
+++ messenger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace messenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
